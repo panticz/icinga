dbaef4eec7ed25e27f6af0e0efda5c0d56f9bf6e
panticz <mail@konczalski.de>
2014-11-16 15:41:19 +0100

    install perl

--- plugins/scripts/check_mem.sh
+++ plugins/scripts/check_mem.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+apt-get install -y perl
 wget -q --no-check-certificate https://raw.github.com/justintime/nagios-plugins/master/check_mem/check_mem.pl -O /usr/lib/nagios/plugins/check_mem
 chmod a+x /usr/lib/nagios/plugins/check_mem
